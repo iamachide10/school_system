369074aa36127f05a37cd29fb6a5a0417207067e
john <iamachide10@gmail.com>
2025-12-12 10:28:34 +0000

    deployment mode

--- school-finance-frontend/src/Pages/landingPage.jsx
+++ school-finance-frontend/src/Pages/landingPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect,useState } from "react";
 import { isAuthenticated } from "../context/authContext";
 import { useNavigate } from "react-router-dom";
-import FullScreenLoader from "../../components/loader";
+import FullScreenLoader from "../components/loader";
 
 
 
