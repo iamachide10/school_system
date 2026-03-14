37b54766e40280cded6feb18ab1749f241e56fb5
john <iamachide10@gmail.com>
2026-03-14 07:21:47 +0000

    record scores feature

--- school-finance-backend/src/config/db.js
+++ school-finance-backend/src/config/db.js
@@ -1,32 +1,28 @@
 import pg from "pg"
-
 import dotenv from "dotenv";
 
-const {Pool}=pg;
+const { Pool } = pg;
 
 dotenv.config()
 
-const credentials={
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
+const isLocal = process.env.FRONTEND_URL === "http://localhost:5173"
+
+const credentials = {
+    user:     process.env.DB_USER,
+    host:     process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    //ssl: { rejectUnauthorized: false }
+    port:     process.env.DB_PORT,
+    ssl:      isLocal ? false : { rejectUnauthorized: false }
 }
 
 const pool = new Pool(credentials)
 
-
-try{
+try {
     pool.connect()
     console.log("Connected Successfully.")
-}catch(error){
-    console.error("Database connnection failed :", error)
+} catch (error) {
+    console.error("Database connection failed :", error)
 }
 
-export default pool 
-
-
-
- 
+export default pool
